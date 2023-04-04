e08ae3be32ec5aaf5c6e3996b372d801ea6503d5
root <root@vps-7f2cb6fb.vps.ovh.net>
2023-04-04 20:53:30 +0000

    Add Webpack encore

--- src/Form/BlogType.php
+++ src/Form/BlogType.php
@@ -8,6 +8,7 @@ use Symfony\Component\Form\Extension\Core\Type\ChoiceType;
 use Symfony\Component\Form\Extension\Core\Type\TextType;
 use Symfony\Component\Form\FormBuilderInterface;
 use Symfony\Component\OptionsResolver\OptionsResolver;
+use FOS\CKEditorBundle\Form\Type\CKEditorType;
 
 class BlogType extends AbstractType
 {
@@ -15,7 +16,11 @@ class BlogType extends AbstractType
     {
         $builder
             ->add('title')
-            ->add('content')
+            ->add('content', CKEditorType::class, [
+                'config' => [
+                    'uiColor' => '#ffffff',
+                ],
+            ])
             ->add('id_author', ChoiceType::class, [
                  'choices' => array_flip( Blog::AUTHOR)
             ])
